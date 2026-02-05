662e20d3135f050e6f40653e38bc35fcd5b52153
Deathkuroneko <deathkuroneko@hotmail.com>
2026-02-05 18:43:48 -0500

    Taller 36 - Guardian de Vue

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import LoginView from '@/views/LoginView.vue';
 
 const routes = [
   { path: '/', redirect: '/login' }, // Si entran a la raíz, mándalos al login
-  { path: '/login', name: 'login', component: LoginView },
+  { path: '/login', name: 'login', component: LoginView, meta:{ requiereAutorizacion: false, esPublica: false} },
   { path: '/about', name: 'about', component: AboutView, meta:{ requiereAutorizacion: true, esPublica: false} },
   { path: '/crear', component: CrearView, meta:{ requiereAutorizacion: true, esPublica: false} },
   { path: '/actualizar', component: ActualizarView, meta:{ requiereAutorizacion: true, esPublica: false} },
@@ -25,7 +25,15 @@ const router = createRouter({
 
 /*Configuracion del guardian */
 router.beforeEach((to, from, next) => {
-  
+  if(to.meta.requiereAutorizacion){
+    /* le envio una pagina al login */
+    console.log("Redirige a login")
+  } else {
+    /* le dejo sin validaciones*/
+    console.log("Pase Libre")
+    next();
+  }
+
 })
 
 
